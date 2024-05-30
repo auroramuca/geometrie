24fce0edcd92329eb72a2f2028b144800a90509b
ErtMuh18 <ertmuh18@gmail.com>
2024-05-30 10:07:33 +0200

    Funktion calculate_square_surphace um die Fläche eines Quadrats zu berechnen.

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static int calculate_square_surphace(int a){
+        int result = a*a;
+        return result;
+    }
 }
