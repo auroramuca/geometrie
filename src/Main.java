4c91e0d6af77344deb05e604d3eaad96937378ae
Aurora Muca <aurmuc18@htl-shkoder.com>
2024-05-30 10:06:17 +0200

    Funktion Dreieck!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+    }
+    public static int triangle(int b, int h){
+        int res;
+        return res = (b*h)/2;
     }
 }
