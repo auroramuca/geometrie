69fc6c202d8a459529fcafef13208542f95286ae
Amanda Isufi <amaisu18@htl-shkoder.com>
2024-05-30 10:10:24 +0200

    Funktion Rechteck!

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        class Rectangle {
 
-            public static double calculateSurface(double length, double width) {
-                return length * width;
-            }
 
-            public static void main(String[] args) {
-                double length = 5;
-                double width = 3;
-                double surfaceArea = calculateSurface(length, width);
-                System.out.println("Die Flaeche ist: " + surfaceArea);
-            }
-        }
-
+    }
+    public static double calculateSurface(double length, double width) {
+        return length * width;
     }
 }
